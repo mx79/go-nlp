784891d70ff6060e3c138510fb508b3813f54cd9
mx79 <lesage.max@sfr.fr>
2022-11-05 15:38:02 +0100

    regex update insensitive matching

--- extractor/regex.go
+++ extractor/regex.go
@@ -15,71 +15,6 @@ type Extractor interface {
 	GetSentences()
 }
 
-//go:embed regex.json
-var regexBytes []byte
-
-// regex
-var regex = loadRegex(regexBytes)
-
-// DefaultRegexExtractor object
-// In information retrieval, sometime we want to extract entities
-// from a sentence or a text, that is where this object can be useful
-// by implementing regular expression extraction from a map
-type DefaultRegexExtractor map[string][]string
-
-// loadRegex loads the map that contains the stopwords in many languages
-func loadRegex(b []byte) DefaultRegexExtractor {
-	var dict DefaultRegexExtractor
-	utils.Check(json.Unmarshal(b, &dict))
-	return dict
-}
-
-// NewDefaultRegexExtractor instantiates a new DefaultRegexExtractor object
-func NewDefaultRegexExtractor() DefaultRegexExtractor {
-	// Return an instance of RegexExtractor
-	return regex
-}
-
-// GetEntity allows us to get back any entity and
-// their corresponding matching pattern from our RegexExtractor dict
-func (ext DefaultRegexExtractor) GetEntity(s string) map[string]interface{} {
-	res := make(map[string]interface{})
-	var patternList []string
-	for k, v := range ext {
-		for _, pattern := range v {
-			re := regexp.MustCompile(pattern)
-			if match := re.FindString(s); match != "" {
-				patternList = append(patternList, match)
-				patternList = utils.SortedSet(patternList)
-				if len(patternList) > 1 {
-					res[k] = patternList
-				} else {
-					res[k] = match
-				}
-			}
-		}
-		patternList = []string{}
-	}
-	return res
-}
-
-// GetSentences allows us to get back any sentences
-// that match a pattern from our RegexExtractor dict
-func (ext DefaultRegexExtractor) GetSentences(slice []string) []string {
-	var res []string
-	for _, v := range ext {
-		for _, pattern := range v {
-			for _, val := range slice {
-				re := regexp.MustCompile(pattern)
-				if match := re.FindString(val); match != "" {
-					res = append(res, val)
-				}
-			}
-		}
-	}
-	return res
-}
-
 // RegexExtractor object
 // In information retrieval, sometime we want to extract entities
 // from a sentence or a text, that is where this object can be useful
@@ -88,7 +23,7 @@ type RegexExtractor map[string][]string
 
 // NewRegexExtractor instantiates a new RegexExtractor object
 func NewRegexExtractor(regexFilePath string) RegexExtractor {
-	// Opening the extractor.json file from the datapath
+	// Opening the provided json
 	jsonFile, err := os.Open(regexFilePath)
 	utils.Check(err)
 	defer jsonFile.Close()
@@ -107,7 +42,7 @@ func (ext RegexExtractor) GetEntity(s string) map[string]interface{} {
 	var patternList []string
 	for k, v := range ext {
 		for _, pattern := range v {
-			re := regexp.MustCompile(pattern)
+			re := regexp.MustCompile("(?i)" + pattern)
 			if match := re.FindString(s); match != "" {
 				patternList = append(patternList, match)
 				patternList = utils.SortedSet(patternList)
